07bd8db0c82fe27a9c45975b54ba782baf21e949
zhangyanxiao <385584895@qq.com>
2017-07-21 16:24:38 +0800

    悬浮html

---
+++ xuanfu.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+		var btn = document.getElementById("closebutton"); 
+       
+		btn.onclick = function(){
+			document.getElementById("xuanfudiv").style.display ="none";
+			
+		}
+				
+}
